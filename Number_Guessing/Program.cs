7d14b0a453b2739841197b47ea1731c71e4611ee
gandzia44 <gamza.szymon@gmail.com>
2021-03-18 02:48:04 +0100

    Added handling other numbers than between desired

--- Number_Guessing/Program.cs
+++ Number_Guessing/Program.cs
@@ -8,45 +8,53 @@ namespace Number_Guessing
         {
             int x,y;
             string z;
-            int a = 0;
-            int b = 100;
-            int i = 0;
+            int a = 0; // First number of between
+            int b = 100; // Last number of between
+            int i = 0; // Iteration counter
             Random random = new();
             x = random.Next(a, b+1); // +1 because <maxValue
             bool win = false;
+            bool parse = false;
             Console.WriteLine($"Try guess number from {a} to {b}");
             do
             {
                 do
                 {
                     z = Console.ReadLine();
-                    if (!Int32.TryParse(z, out y))
+                    parse = Int32.TryParse(z, out y);
+                    if (parse == false)
                     {
                         Console.WriteLine("USE NUMBER!");
-                        y = -1;
                     }
                     else
                     {
                         y = Int32.Parse(z);
                     }
-                } while (y == -1);
+                } while (parse == false);
 
-                i++;
 
-                if (y > x)
+
+                if (y < a || y > b)
                 {
-                    Console.WriteLine("Wrong number! Try lower");
+                    Console.WriteLine($"Number should be between {a} and {b}");
                 }
-                else if (y < x)
+                else
                 {
-                    Console.WriteLine("Wrong number! Try higher");
+                    i++;
+                    if (y > x)
+                    {
+                        Console.WriteLine("Wrong number! Try lower");
+                    }
+                    else if (y < x)
+                    {
+                        Console.WriteLine("Wrong number! Try higher");
+                    }
+                    else if (y == x)
+                    {
+                        Console.WriteLine($"Right number! You Won on {i} attempt");
+                        win = true;
+                    }
                 }
-                else if (y == x)
-                {
-                    Console.WriteLine($"Right number! You Won on {i} attempt");
-                    win = true;
-                }
-                
             } while (win == false);
             Console.WriteLine();
             Console.WriteLine("Thank you for playing my game!");
